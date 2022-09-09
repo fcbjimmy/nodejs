44e6b86cc2db3f9ca606a671626d60fcb3f4765f
fcbjimmy <jim.culers@gmail.com>
2022-09-09 17:27:17 +0800

    deleted

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,27 +1,54 @@
 const express = require("express");
 const app = express();
+const { products } = require("./data");
 
 app.get("/", (req, res) => {
-  console.log("User hit the resource");
-  res.status(200).send("Home page");
+  res.send('<h1> Home Page</h1><a href="/api/products">products</a>');
 });
 
-app.get("/about", (req, res) => {
-  res.status(200).send("About page");
+app.get("/api/products/", (req, res) => {
+  const newProduct = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.send(newProduct);
 });
 
-app.all("*", (req, res) => {
-  res.status(404).send("<h1>Resource not found</h1>");
+app.get("/api/products/:ProductID", (req, res) => {
+  const { ProductID } = req.params;
+
+  const singleProduct = products.find(
+    (product) => product.id === Number(ProductID)
+  );
+
+  if (!singleProduct) {
+    return res.status(404).send("Page not found");
+  }
+
+  return res.send(singleProduct);
+});
+
+app.get("/api/v1/query", (req, res) => {
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
+
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+
+  if (sortedProducts.length < 1) {
+    // res.status(200).send("no products matched for your search");
+    return res.status(200).json({ success: true, data: [] });
+  }
+  res.status(200).json(sortedProducts);
 });
 
 app.listen(4000, () => {
   console.log("Server is listening on port 4000");
 });
-
-// app.get
-// app.post
-// app.put
-// app.delete
-// app.all
-// app.use
-// app.listen
